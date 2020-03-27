4730baea87419ffa4f5be3429b2b10a9f58b13eb
Tom Banish <tombanish@gmail.com>
2020-03-27 20:25:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,44 +3,45 @@ require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
-  def self.create
-    song = self.new
-    song.save
-    song
-  end
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
   
-  def self.new_by_name(song_name)
+  def self.create
     song = self.new
-    song.name = song_name
+    @@all << song
     song
   end
   
-  def self.create_by_name(song_name)
+  def self.new_by_name(name)
     song = self.new
-    song.name = song_name
-    song.save
+    song.name = name
     song
   end
   
-  def self.find_by_name(song_name)
-    @@all.find {|s| s.name == song_name}
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
   end
   
-  def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
   
   def self.alphabetical
-    @@all.sort_by {|s| s.name}
+    songs_sorted= self.all.sort_by {|word| word.name}
+    songs_sorted
   end
   
   def self.new_from_filename(filename)
@@ -51,15 +52,12 @@ class Song
   end
   
   def self.create_from_filename(filename)
-    song = self.new
-    song.name = filename.split(" - ")[1].chomp(".mp3")
-    song.artist_name = filename.split(" - ")[0]
+    song = self.new_from_filename(filename)
     song.save
     song
   end
-
-  def self.destroy_all
-    @@all.clear
-  end
   
+  def self.destroy_all
+    self.all.clear
+  end
 end
